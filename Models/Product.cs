8296b737f616f98c354a0d616d22f5d13815fdb9
kurie139266 <kurie139266@gmail.com>
2026-07-02 14:14:05 +0900

    Initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+namespace POSSystem.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public int Price { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using POSSystem.Models;
+
+List<Product> products = new()
+{
+    new Product { Id = 1, Name = "おにぎり", Price = 150 },
+    new Product { Id = 2, Name = "パン", Price = 220 },
+    new Product { Id = 3, Name = "お茶", Price = 180 }
+};
+
+Console.WriteLine("===== POSシステム =====");
+
+foreach (var product in products)
+{
+    Console.WriteLine($"{product.Id}. {product.Name} {product.Price}円");
+}
